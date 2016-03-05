488d0029b18c57d69b5af9c9c2e7fa8124327839
Theophile Villard <theophile.villard@gmail.com>
2016-03-05 19:20:07 +0000

    config: fix domain url I'm a genius...

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const DOMAIN = 'http://bornycen.re'
+export const DOMAIN = 'http://bornycent.re'
 export const MAPS_KEY = 'AIzaSyAdqiipKq9po0jxbrKKZ7p-YWzYmKQsxKU'
 export const MAPS_EMBED_URL = 'https://www.google.com/maps/embed/v1/place'
