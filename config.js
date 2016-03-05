2c89f6f1fa8e29265bf9391b9ce766b914caa75f
Theophile Villard <theophile.villard@gmail.com>
2016-03-05 10:56:12 +0000

    main: refactor with mvi in mind

---
+++ config.js
@@ -0,0 +1,2 @@
+export const MAPS_KEY = 'AIzaSyAdqiipKq9po0jxbrKKZ7p-YWzYmKQsxKU'
+export const MAPS_EMBED_URL = 'https://www.google.com/maps/embed/v1/place'
--- main.js
+++ main.js
@@ -2,12 +2,12 @@ import Cycle from '@cycle/core'
 import {makeDOMDriver, div, input, ol, li, a, iframe, label, i} from '@cycle/dom'
 import {makeHTTPDriver} from '@cycle/http'
 import {Observable} from 'rx'
+import {cloneDeep} from 'lodash'
+import {MAPS_KEY, MAPS_EMBED_URL} from './config.js'
 
-const MAPS_KEY = 'AIzaSyAdqiipKq9po0jxbrKKZ7p-YWzYmKQsxKU'
-const MAPS_EMBED_URL = 'https://www.google.com/maps/embed/v1/place'
-
-function main (sources) {
-  const request$ = sources.DOM.select('.js-name-input').events('input')
+function createRequestCitiesStream (DOM) {
+  const request$ = DOM.select('.js-name-input')
+    .events('input')
     .debounce(500)
     .map(e => e.target.value)
     .filter(query => query.length > 0)
@@ -18,52 +18,112 @@ function main (sources) {
         category: 'geonames'
       }
     })
+  return request$
+}
 
-  const selectCity$ = sources.DOM.select('.js-result').events('click')
-    .map(e => {
-      return {
-        name: e.target.getAttribute('data-name'),
-        country: e.target.getAttribute('data-country'),
-        lat: e.target.getAttribute('data-lat'),
-        lng: e.target.getAttribute('data-lng')
-      }
-    })
+function createSelectCityStream (DOM) {
+  const selectCity$ = DOM.select('.js-result')
+    .events('click')
+    .map(e => cloneDeep(e.target.city)) // lot of pain without the cloneDeep
+  return selectCity$
+}
 
-  const selectedCities$ = selectCity$
+function createReceiveCitiesStream (HTTP) {
+  const cities$ = HTTP
+    .filter(res$ => res$.request.category === 'geonames')
+    .flatMap(x => x)
+    .filter(res => res.body && res.body.geonames)
+    .map(res => res.body.geonames)
+  return cities$
+}
+
+function computeSelectedCitiesStream (city$) {
+  const selectedCities$ = city$
     .startWith([])
     .scan((cities, city) => cities.concat(city))
+  return selectedCities$
+}
 
-  const bornycentre$ = selectedCities$
-    .filter(cities => cities.length > 0)
-    .map(cities => {
-      const count = cities.length
-      const sumObject = cities.reduce((sum, city) => {
-        return {
-          lat: sum.lat + parseFloat(city.lat),
-          lng: sum.lng + parseFloat(city.lng)
-        }
-      }, {lat: 0, lng: 0})
-      return {
-        lat: sumObject.lat / count,
-        lng: sumObject.lng / count
+function computeReturnedCitiesStream (selectedCities$, cities$) {
+  const returnedCities = Observable.combineLatest(selectedCities$, cities$,
+    (selectedCities, cities) =>
+      cities.filter(city => selectedCities.every(c => c.name !== city.name))
+    )
+    .startWith([])
+  return returnedCities
+}
+
+function computeBornycentreStream (city$) {
+  const bornycentre$ = city$
+    .startWith({sumLat: 0, sumLng: 0, count: 0})
+    .scan((bornycentre, city) => {
+      const newBorn = {
+        sumLat: bornycentre.sumLat + parseFloat(city.lat),
+        sumLng: bornycentre.sumLng + parseFloat(city.lng),
+        count: bornycentre.count + 1
       }
+      return Object.assign({
+        lat: newBorn.sumLat / newBorn.count,
+        lng: newBorn.sumLng / newBorn.count
+      }, newBorn)
     })
-    .startWith(null)
+  return bornycentre$
+}
 
-  const selectedVTree$ = Observable.combineLatest(selectedCities$, bornycentre$,
-    (cities, bornycentre) => {
+function renderForm (returnedCities$) {
+  const vtree$ = returnedCities$.map(returnedCities => {
+    return (
+      div('.col .s12', [
+        div('.row', [
+          div('.input-field .col .s6 .push-s3', [
+            input('.js-name-input', {id: 'name-input', type: 'text'}),
+            label({attributes: {for: 'name-input'}}, 'Add your cities')
+          ])
+        ]),
+        div('.row', [
+          div('.js-results .col .s8 .push-s2 .center-align',
+            {style: {minHeight: '100px'}},
+            returnedCities
+              .map(city =>
+                a('.js-result .btn', {
+                  style: {margin: '5px'},
+                  city: city
+                },
+                  `${city.name}, ${city.countryName}`
+                )
+              )
+            )
+        ])
+      ])
+    )
+  })
+  return vtree$
+}
+
+function renderSelectedCities (cities$) {
+  const vtree$ = cities$
+    .map(cities => {
       if (cities.length === 0) {
         return null
       }
-      let vContent = [ol(
-        '.collection',
-        cities.map(city =>
-          li('.collection-item', `${city.name}, ${city.country}`)
-        )
-      )]
-      if (bornycentre !== null) {
-        const coords = `${bornycentre.lat},${bornycentre.lng}`
-        vContent.push(div('.center-align', [
+      return (
+        ol('.collection', cities.map(city =>
+          li('.collection-item', `${city.name}, ${city.countryName}`)
+        ))
+      )
+    })
+  return vtree$
+}
+
+function renderBornycentre (bornycentre$) {
+  const vtree$ = bornycentre$
+    .map(bornycentre => {
+      if (bornycentre.count === 0) {
+        return null
+      }
+      const coords = `${bornycentre.lat},${bornycentre.lng}`
+      return (
+        div('.center-align', [
           a('.btn-large .indigo',
             {
               href: `https://www.google.com/maps/place/${coords}`,
@@ -83,66 +143,37 @@ function main (sources) {
               style: {border: 0}
             })
           ])
-        ]))
-      }
-      return vContent
-    }
-  )
-
-  const cities$ = sources.HTTP
-    .filter(res$ => res$.request.category === 'geonames')
-    .flatMap(x => x)
-    .filter(res => res.body && res.body.geonames)
-    .map(res => res.body.geonames)
-    .startWith([])
-
-  const nameSelectedCities$ = selectedCities$.map(cities => {
-    let names = {}
-    cities.forEach(city => names[city.name] = true)
-    return names
-  })
-
-  const formVTree$ = Observable.combineLatest(cities$, nameSelectedCities$,
-    (cities, nameSelectedCities) => {
-      return cities$.map(cities =>
-        div('.col .s12', [
-          div('.row', [
-            div('.input-field .col .s6 .push-s3', [
-              input('.js-name-input', {id: 'name-input', type: 'text'}),
-              label({attributes: {for: 'name-input'}}, 'Add your cities')
-            ])
-          ]),
-          div('.row', [
-            div('.js-results .col .s8 .push-s2 .center-align',
-              {style: {minHeight: '100px'}},
-              cities
-                .filter(city => !nameSelectedCities[city.name])
-                .map(city =>
-                  a('.js-result .btn', {
-                    style: {margin: '5px'},
-                    attributes: {
-                      'data-name': city.name,
-                      'data-country': city.countryName,
-                      'data-lat': city.lat,
-                      'data-lng': city.lng
-                    }},
-                    `${city.name}, ${city.countryName}`
-                  )
-                )
-              )
-          ])
         ])
       )
-    }
-  )
+    })
+  return vtree$
+}
 
-  const vtree$ = Observable.combineLatest(formVTree$, selectedVTree$,
-    (formVTree, selectedVTree) => div('.row', [formVTree, selectedVTree])
+function wrap (...arrayVTree$) {
+  const vtree$ = Observable.combineLatest(arrayVTree$,
+    (...arrayVTree) => div('.row', arrayVTree)
   )
+  return vtree$
+}
+
+function main (sources) {
+  // intent -> action
+  const requestCities$ = createRequestCitiesStream(sources.DOM)
+  const receiveCities$ = createReceiveCitiesStream(sources.HTTP)
+  const selectCity$ = createSelectCityStream(sources.DOM)
+  // action -> state
+  const selectedCities$ = computeSelectedCitiesStream(selectCity$)
+  const returnedCities$ = computeReturnedCitiesStream(selectedCities$, receiveCities$)
+  const bornycentre$ = computeBornycentreStream(selectCity$)
+  // state -> view
+  const formVTree$ = renderForm(returnedCities$)
+  const selectedVTree$ = renderSelectedCities(selectedCities$)
+  const bornycentreVTree$ = renderBornycentre(bornycentre$)
+  const vtree$ = wrap(formVTree$, selectedVTree$, bornycentreVTree$)
 
   return {
     DOM: vtree$,
-    HTTP: request$
+    HTTP: requestCities$
   }
 }
 
